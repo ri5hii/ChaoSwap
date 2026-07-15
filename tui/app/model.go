8bcda285c1e12bdd73a812126480b2b8a842cc18
ri5hii <rishi.0w0@gmail.com>
2026-07-15 21:53:53 +0530

    defined basic model structs for bubble tea

--- tui/app/model.go
+++ tui/app/model.go
@@ -1,6 +1,41 @@
 package app
 
-type Model struct{}
+import (
+	engineChaos "github.com/ri5hii/ChaoSwap/engine/engine-chaos"
+	engineNormal "github.com/ri5hii/ChaoSwap/engine/engine-normal"
+)
 
-type MoveRecord struct{}
+type Model struct {
+	chessboard *engineNormal.BoardState
+	chaos      *engineChaos.State
+	mode       string
+	input      string
+	status     string
+	helpLine   string
+
+	moveLog []MoveRecord
+
+	summary SummaryState
+}
+
+type MoveRecord struct {
+	pieceType engineNormal.PieceType
+	Side      engineNormal.PieceColor
+	To        engineNormal.Square
+	From      engineNormal.Square
+
+	Ply int
+
+	isCapture         bool
+	isQueenSideCastle bool
+	isKingSideCastle  bool
+
+	isPromotion   bool
+	PromotionType engineNormal.PieceType
+}
+
 type SummaryState struct{}
+
+type Banner struct {
+	gameBanner string
+}
